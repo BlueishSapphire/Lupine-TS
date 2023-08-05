014cdbb33bf317479a573a826e26446b2c2f7c11
sapphire <blueishsapphire0@gmail.com>
2023-08-04 21:53:23 -0400

    Now uses the parser

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { tokenize } from "./lexer";
 import * as fs from "node:fs";
+import { Parser } from "./parser";
 
 
 
-let contents = fs.readFileSync("./test.lup").toString();
-console.dir(tokenize(contents));
+const contents = fs.readFileSync("./test.lup").toString();
+const tokens = tokenize(contents);
+
+const parser = new Parser(tokens);
+const ast = parser.parse();
