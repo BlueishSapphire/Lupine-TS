58dbd3d08164d192b4f1c066a98fe14c23b0839f
sapphire <blueishsapphire0@gmail.com>
2023-08-05 16:04:02 -0400

    Error handling in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { Parser } from "./parser";
 
 
 const targetFileName = process.argv[2];
+if (targetFileName === undefined || targetFileName.length === 0) {
+	console.log("Please run this command with the filename of the script you want to compile");
+	process.exit(1);
+}
+
 const contents = fs.readFileSync(targetFileName).toString();
 const tokens = tokenize(contents);
 
